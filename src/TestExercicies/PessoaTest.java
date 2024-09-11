158a7a61b7ec9da81258c37bed353d143c19e21a
nbeverton <104376567+nbeverton@users.noreply.github.com>
2024-09-11 18:57:56 -0300

    Update PessoaTest.java
    New Test.

--- src/TestExercicies/PessoaTest.java
+++ src/TestExercicies/PessoaTest.java
@@ -7,9 +7,15 @@ import java.time.LocalDateTime;
 
 public class PessoaTest {
 
-@Test
+    @Test
+    void deveCalcularIdadeCorretamente(){
+        Pessoa pessoa2 = new Pessoa("João Banana", LocalDateTime.of(1991, 01, 01, 01, 01));
+        Assertions.assertEquals(33, pessoa2.getIdade());
+    }
+
+    @Test
     void deveRetornarSeMaiorIdade(){
         Pessoa pessoa1 = new Pessoa("Everton", LocalDateTime.of(1991, 06, 10, 05, 38));
-    Assertions.assertTrue(pessoa1.ehMaiorIdade());
+        Assertions.assertTrue(pessoa1.ehMaiorIdade());
     }
 }
