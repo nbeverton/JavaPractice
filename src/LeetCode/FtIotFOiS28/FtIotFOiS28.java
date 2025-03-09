6f4b9e025aa4f9c09fa1ec0ebc93a384c59362ab
nbeverton <104376567+nbeverton@users.noreply.github.com>
2025-03-09 16:03:22 -0300

    Create FtIotFOiS28.java

---
+++ src/LeetCode/FtIotFOiS28/FtIotFOiS28.java
@@ -0,0 +1,23 @@
+package LeetCode.FtIotFOiS28;
+
+public class FtIotFOiS28 {
+
+    public int strStr(String haystack, String needle){
+
+        int occurrence = 0;
+
+        if (haystack.contains(needle)){
+            occurrence = haystack.indexOf(needle);
+        } else {
+            occurrence = -1;
+        }
+
+        return occurrence;
+    }
+
+    public static void main(String[] args) {
+        FtIotFOiS28 ftIotFOiS28 = new FtIotFOiS28();
+        
+        System.out.println(ftIotFOiS28.strStr("everton", "ton"));
+    }
+}
