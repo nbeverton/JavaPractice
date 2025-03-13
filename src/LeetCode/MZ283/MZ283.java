c6930b8e19d928ee27b86a82bd55d94df0be9dad
nbeverton <104376567+nbeverton@users.noreply.github.com>
2025-03-13 12:03:23 -0300

    Create MZ283.java

---
+++ src/LeetCode/MZ283/MZ283.java
@@ -0,0 +1,31 @@
+package LeetCode.MZ283;
+
+import java.util.Arrays;
+
+public class MZ283 {
+    public void moveZeros(int[] nums){
+
+        int numsLength = nums.length;
+        int numsNomZeroPosition = 0;
+
+        for (int i = 0; i < numsLength; i++) {
+
+            if (nums[i] != 0){
+                nums[numsNomZeroPosition] = nums[i];
+                numsNomZeroPosition++;
+            }
+        }
+
+        for (int i = numsNomZeroPosition; i < numsLength; i++) {
+            nums[i] = 0;
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void main(String[] args) {
+        MZ283 mz283 = new MZ283();
+
+        int[] nums = {0,1,0,3,12};
+        mz283.moveZeros(nums);
+    }
+}
