c8fb16d0bebe59b56d9581a44f6971f3a0bb58cf
nbeverton <104376567+nbeverton@users.noreply.github.com>
2025-03-25 21:30:14 -0300

    Create RtI_13.java

---
+++ src/LeetCode/RtI_13/RtI_13.java
@@ -0,0 +1,67 @@
+package LeetCode.RtI_13;
+
+public class RtI_13 {
+
+    public int romanToInt(String s){
+
+        int result = 0;
+        char[] sChar = s.toCharArray();
+
+
+        for (int i = 0; i < sChar.length; i++) {
+            if (i < sChar.length - 1){
+
+                if ( sChar[i] == 'I' && sChar[i + 1] == 'V'  ) {
+                    result += 4;
+                    i++;
+                    continue;
+                }
+                if (sChar[i] == 'I' && sChar[i + 1] == 'X') {
+                    result  += 9;
+                    i++;
+                    continue;
+                }
+                if (sChar[i] == 'X' && sChar[i + 1] == 'L') {
+                    result  += 40;
+                    i++;
+                    continue;
+                }
+                if (sChar[i] == 'X' && sChar[i + 1] == 'C') {
+                    result  += 90;
+                    i++;
+                    continue;
+                }
+                if (sChar[i] == 'C' && sChar[i + 1] == 'D') {
+                    result  += 400;
+                    i++;
+                    continue;
+                }
+                if (sChar[i] == 'C' && sChar[i + 1] == 'M') {
+                    result  += 900;
+                    i++;
+                    continue;
+                }
+
+            }
+
+                switch (sChar[i]) {
+                    case 'I' -> result += 1;
+                    case 'V' -> result += 5;
+                    case 'X' -> result += 10;
+                    case 'L' -> result += 50;
+                    case 'C' -> result += 100;
+                    case 'D' -> result += 500;
+                    case 'M' -> result += 1000;
+                }
+
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        RtI_13 rtI13 = new RtI_13();
+
+        System.out.println(rtI13.romanToInt("MCMXCIV"));
+    }
+}
