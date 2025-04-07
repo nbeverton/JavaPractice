50f7d29412fad7a7f0444a00b27ab8746d930298
nbeverton <104376567+nbeverton@users.noreply.github.com>
2025-04-07 17:28:29 -0300

    Update LPTriangle_976.java

--- src/LeetCode/LPTriangle_976/LPTriangle_976.java
+++ src/LeetCode/LPTriangle_976/LPTriangle_976.java
@@ -1,30 +1,21 @@
 package LeetCode.LPTriangle_976;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class LPTriangle_976 {
     public int largestPerimeter(int[] nums){
         Arrays.sort(nums);
 
-        int lastValueIndex = nums.length - 1;
-        int lastValue = nums[nums.length - 1];
-
-        System.out.println("lastValueIndex: " + lastValueIndex);
-        System.out.println("lastValue: " + lastValue);
-
-        for (int i = 0; i < nums.length - 2; i++) {
-
-            if (nums.length == 3){
-                if (nums[i] + nums[i + 1] > lastValue){
-                    return lastValue + nums[i] + nums[i + 1];
-                } else return 0;
+        for (int i = nums.length - 1; i >= 2; i--) {
+            int a = nums[i - 2];
+            int b = nums[i - 1];
+            int c = nums[i];
+            
+            if (a + b > c) {
+                return a + b + c;
             }
-
-            if (lastValue < nums[lastValueIndex - 1] + nums[lastValueIndex - 2]){
-                return lastValue + nums[lastValueIndex - 1] + nums[lastValueIndex - 2];
-            } else if(lastValue - i < nums[lastValueIndex - 1 - i] + nums[lastValueIndex - 2 - i]) {
-                return lastValue + nums[lastValueIndex - 1 - i] + nums[lastValueIndex - 2 - i];
-            }
-
         }
 
         return 0;
@@ -33,9 +24,7 @@ public class LPTriangle_976 {
     public static void main(String[] args) {
         LPTriangle_976 triangle976 = new LPTriangle_976();
 
-        int[] input1 = {3,6,2,3};
-        int[] input2 = {2,1,2};
-        int[] input3 = {1,2,1,10};
+        int[] input1 = {1,2,2,4,18,8};
         System.out.println(triangle976.largestPerimeter(input1));
     }
 }
