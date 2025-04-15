2ba4ed43d554216c09c21b910dc19c2e5c07b0e7
nbeverton <104376567+nbeverton@users.noreply.github.com>
2025-04-15 16:01:29 -0300

    Update PalidromeNumber_9.java
    Updated to correct.

--- src/LeetCode/Abril/PalidromeNumber_9/PalidromeNumber_9.java
+++ src/LeetCode/Abril/PalidromeNumber_9/PalidromeNumber_9.java
@@ -3,19 +3,24 @@ package LeetCode.Abril.PalidromeNumber_9;
 public class PalidromeNumber_9 {
     public boolean isPalidrome(int x){
 
-        if (x < 0){
-            return false;
+        if (x < 0) return false;
+        if (x == 0) return true;
+
+        StringBuilder xString = new StringBuilder();
+
+        int value = x;
+        while(value > 0){
+            xString.append(value % 10);
+            value = value / 10;
         }
 
-        String xStringReversed = new StringBuilder(Integer.toString(x)).reverse().toString();
-        int xReversed = Integer.parseInt(xStringReversed);
-        return xReversed == x;
+        return Integer.toString(x).contentEquals(xString);
     }
 
     public static void main(String[] args) {
         PalidromeNumber_9 palidromeNumber9 = new PalidromeNumber_9();
 
-        int input = 1234567892;
+        int input = 1234321;
         System.out.println(palidromeNumber9.isPalidrome(input));
     }
 }
